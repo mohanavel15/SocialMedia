5ace6fdf1f8091f93a626f232da5cfb62c4e09bf
Mohan <mohanavel15@protonmail.com>
2022-11-11 14:29:46 +0530

    solid router setup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,22 @@
-import type { Component } from 'solid-js';
+import { Route, Routes } from '@solidjs/router';
+import { Component, lazy } from 'solid-js';
 
+const Home = lazy(() => import("./pages/Home"));
+const User = lazy(() => import("./pages/User"));
+const Post = lazy(() => import("./pages/Post"));
+const Login = lazy(() => import("./pages/Login"));
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-    </div>
+    <>
+      <h1>My Site with Lots of Pages</h1>
+      <Routes>
+        <Route path="/" component={Home} />
+        <Route path="/:id" component={User} />
+        <Route path="posts/:id" component={Post} />
+        <Route path="/login" component={Login} />
+      </Routes>
+    </>
   );
 };
 
