ed79f8b6f9d8a78eea77fe80e264def17e7875f2
Mohan <mohanavel15@protonmail.com>
2023-01-07 12:04:19 +0530

    fetching feed includes current user's posts

--- server/database/feed.go
+++ server/database/feed.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -24,7 +25,11 @@ func GetFeed(user User, limit int64, offset int64) []Post {
 	posts := []Post{}
 	postCollection := Mongo.Collection("posts")
 
-	cur, err := postCollection.Find(context.TODO(), bson.M{"author_id": bson.M{"$in": user.Following}}, options.Find().SetSort(bson.M{"created_at": -1}).SetLimit(limit).SetSkip(offset))
+	following := []primitive.ObjectID{}
+	following = append(following, user.ID)
+	following = append(following, user.Following...)
+
+	cur, err := postCollection.Find(context.TODO(), bson.M{"author_id": bson.M{"$in": following}}, options.Find().SetSort(bson.M{"created_at": -1}).SetLimit(limit).SetSkip(offset))
 	if err != nil {
 		return posts
 	}
